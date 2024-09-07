de8550cedf6fab23f31022216e930d347638f348
VayerMaking <martin.vayer@gmail.com>
2024-09-07 12:02:23 +0300

    surround getting with try/catch

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ sleep_data = TinyDB('sleep.json')
 
 @app.route('/state')
 def sleep():
-    current_state = state.all()[0]
+
+    try:
+        return state.all()[0]
+    except IndexError:
+        state.insert({'isSleeping': False})
+        return state.all()[0]
+
     state.update({'isSleeping': not current_state['isSleeping']}, Query().isSleeping == current_state['isSleeping'])
     return current_state
 
 @app.route('/sleep', methods=['POST', 'DELETE'])
 def start():
+
+    try:
+        sleep_data.get(doc_id=1)
+    except IndexError:
+        sleep_data.insert({'start': None})
+
     if request.method == 'POST':
         current_time = datetime.datetime.now(pytz.timezone('Europe/Sofia')).timestamp()
         sleep_data.update({'start': current_time})
